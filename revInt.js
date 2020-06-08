5703ddb1688f9b5d9f36d7949461dfe64e630325
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-06-08 09:33:45 -0400

    maybe solution

--- revInt.js
+++ revInt.js
@@ -1,3 +1,28 @@
 function reverseInteger(number) {
     // TODO: Implement this function!  
+    // Cannot turn number into a string or an array
+  
+    let revInt = 0;
+    let lastDigit;
+
+    revMachine(number);
+
+    revMachine = (num) => {
+
+      lastDigit = num % 10; //Get the last digit 
+
+      revInt = (revInt * 10) + lastDigit
+
+      number = number / 10; 
+
+      if(number = 0){
+
+        return revInt;
+
+      } else revMachine(number);
+    };
   }
+
+  //Test Cases
+
+    var test = reverseInteger("1234");
