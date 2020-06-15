66e37929b98dc4ba18df60bc3de6d5d562da7f8b
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-06-15 10:58:03 -0400

    works

--- Nonrepeat/nonrepeat.js
+++ Nonrepeat/nonrepeat.js
@@ -1,4 +1,18 @@
 var firstNonRepeatedCharacter = function (string) {
     // TODO: your solution here
-  
+    //Look for first nonrepeated character
+
+
+    for (let i in string) {
+        console.log(string.lastIndexOf(string[i]))
+        console.log(string.indexOf(string[i]))
+        if (string.lastIndexOf(string[i]) === string.indexOf(string[i])) {
+          return string[i];
+        }
+      }
+      return null;
+
+    
   };
+
+  firstNonRepeatedCharacter("AABDS")
